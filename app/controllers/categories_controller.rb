198e7443e249e90e4bff98121bea6e1da3ab5ea0
JosephPR <reilly4181@gmail.com>
2019-05-27 14:55:46 -0600

    added to categories controller and views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,11 @@
 class CategoriesController < ApplicationController
 
   def index
-    @categories = Category.all 
+    @categories = Category.all
+  end
+
+  def show
+      @category = Category.find(params[:id])
+    @varietals = Varietal.select{|varietal| varietal.category_id == @category.id}
   end
 end
