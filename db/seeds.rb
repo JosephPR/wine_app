db437d58e14e6dce469469d93f442a773b4296dd
JosephPR <reilly4181@gmail.com>
2019-05-28 11:28:17 -0600

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,8 @@ Category.destroy_all
 red = Category.create(name: "Red Wines");
 white = Category.create(name: "White Wines");
 sparkling = Category.create(name: "Sparkling Wines");
+
+
+pinot = Varietal.create(name: "Pinot Grigio", description: "Crispy, Fruity, Refreshing, with hints of apples.", image_url:"https://winefolly.com/wp-content/uploads/2013/08/pinot-grigio-gris-wine-guide.jpg")
+cabernet = Varietal.create(name: "Cabernet Sauvignon", description: "Currant, Plum, Black Cherry & Spice, with notes of Olive, Vanilla Mint, Tobacco, Toasty Cedar, Anise, Pepper & Herbs. Full-bodied wines with great depth that improve with aging. Cabernet spends from 15 to 30 months aging in American & French Oak barrels which tend to soften the tannins, adding the toasty cedar & vanilla flavors.", image_url:"https://winefolly-wpengine.netdna-ssl.com/wp-content/uploads/2012/02/color-of-wine-primary-indicators.jpg")
+proscecco = Varietal.create(name: "Prosecco", description: "dry, fruity, bubbly fun and the king of mimosas!", image_url:"https://static.vinepair.com/wp-content/uploads/2016/01/prosecco-header.jpg")
