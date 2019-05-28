ae3c2c74d6daf2528ea8701a6ad05df486b5e325
brookton <davidbrookton@gmail.com>
2019-05-28 12:34:21 -0600

    form

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -5,16 +5,24 @@ class NotesController < ApplicationController
   end
 
   def new
+    @note = Note.new
+    @varietal = Varietal.all
   end
 
   def create
-
+    @note = Note.create(note_params)
+    if @note.errors.any?
+      @varietals = Varietal.all
+      render :new
+    else
+    redirect_to varietals_path
+    end
   end
 
 
     private
 
-    def notes_params
+    def note_params
       params.require(:note).permit(:sweetness, :acidity, :tanin, :alcohol, :body)
     end
 end
